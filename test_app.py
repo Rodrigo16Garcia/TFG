afbabeafd397805fe492f7eb5c8926c0a4e0e30d
rodrgar <659-rodrgar@users.noreply.gitlab.inf.uva.es>
2025-01-08 01:47:50 +0100

    Langgraph

--- test_app.py
+++ test_app.py
@@ -1,24 +1,47 @@
 import gradio as gr
 import chromadb 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_community.llms import Ollama
+from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 import chromadb
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+
 
 def format_docs(docs: list[Document]):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-llm = Ollama(base_url="http://localhost:11434", model="llama3.1:8b-instruct-q4_K_M")
-llm
+
+llm = ChatOllama(base_url="http://localhost:11434", model="llama3.1:8b-instruct-q4_K_M")
 
 cliente = chromadb.HttpClient("localhost", 7888)
-chroma = Chroma(collection_name="prueba_kafka", embedding_function=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"), client=cliente)
+
+print("conexión")
+
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+chroma = Chroma(collection_name="prueba_kafka", embedding_function=embeddings, client=cliente)
 retriever  = chroma.as_retriever()
+
+# from langchain_core.documents import Document
+# from typing_extensions import List, TypedDict
+
+# class State(TypedDict):
+#     question: str
+#     context: List[Document]
+#     history: list[list[str,str]]
+#     answer: str
+
+class State(TypedDict): 
+    question: str
+    context: List[Document]
+    answer: str
+    history: str 
 
 
 system_prompt = (
@@ -31,13 +54,36 @@ system_prompt = (
     "{context}"
 )
 
+
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
+        ("placeholder", "{history}"),
         ("human", "{input}"),
     ]
 )
 
+def retrieve(state: State):
+    retrieved_docs = chroma.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+def generate(state: State): 
+    docs_content = format_docs(state["context"])
+    messages = prompt.invoke({"input": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response}
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+def adapter(message: str, history: list[list[str,str]]):
+    result = graph.invoke({"question": message, "history": history})
+    return result["answer"].content  + "\nLos documentos originale son los siguientes:\n\t" "\n\t".join([doc.metadata["source"] for doc in result["context"]])
+
+from IPython.display import Image, display
+
+Image(graph.get_graph().draw_mermaid_png())
 
 # prompt = ChatPromptTemplate([
 #         ( """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
@@ -46,29 +92,29 @@ prompt = ChatPromptTemplate.from_messages(
 #          Answer:""")
 # ])
 
-from langchain.globals import set_verbose
-set_verbose(True)
-chain = (
-    {"context": retriever | format_docs, "input": RunnablePassthrough()}
-    | prompt 
-    | llm 
-    | StrOutputParser()
-)   
+# from langchain.globals import set_verbose
+# set_verbose(True)
+# chain = (
+#     {"context": retriever | format_docs, "input": RunnablePassthrough()}    
+#     | prompt 
+#     | llm 
+#     | StrOutputParser()
+# )   
 
 
 
-def predict(message: str, history: list[list[str,str]]):
+# def predict(message: str, history: list[list[str,str]]):
 
-    # print(prompt.({"context": "contexto","input": message}))
+#     # print(prompt.({"context": "contexto","input": message}))
 
-    print(history)
+#     print(history)
     
-    return str(chain.invoke(message))
+#     return str(chain.invoke(message))
 
 
 interface = gr.Chatbot(label="Chat time!")
 with gr.Blocks() as demo:
-    chatbot = gr.ChatInterface(fn=predict, chatbot=interface, title="RAG chatbot de prueba")
+    chatbot = gr.ChatInterface(fn=adapter, chatbot=interface, title="RAG chatbot de prueba", examples=["Hola", "Dime algo sobre Franz Kafka", "Como se debería manejar una clase de 30 alumnos de primaria."])
 
 if __name__ == "__main__":
     demo.launch() 
